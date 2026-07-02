d1eb949ab00e0f0bf080f13e644e8223791a0a82
iankarlhos <iankarlhossolanogarcia@gmail.com>
2026-07-02 09:58:02 -0500

    agrega controlador de autenticación con registro, inicio y cierre de sesión

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ContactController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    Route::get('/contacts', [ContactController::class, 'index']);
+    Route::post('/contacts', [ContactController::class, 'store']);
+    Route::get('/contacts/{contact}', [ContactController::class, 'show']);
+    Route::put('/contacts/{contact}', [ContactController::class, 'update']);
+    Route::patch('/contacts/{contact}', [ContactController::class, 'update']);
+    Route::delete('/contacts/{contact}', [ContactController::class, 'destroy']);
+});
