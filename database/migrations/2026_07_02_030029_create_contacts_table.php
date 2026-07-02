a0bceb0ce606fa01303b354a42c3c51aed1bd87e
iankarlhos <iankarlhossolanogarcia@gmail.com>
2026-07-02 08:53:30 -0500

    actualiza la base de datos y agrega la tabla de contactos

--- database/migrations/2026_07_02_030029_create_contacts_table.php
+++ database/migrations/2026_07_02_030029_create_contacts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('phone_number');
             $table->timestamps();
         });
     }
